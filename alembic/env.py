ab5ad72ae7e7cc6fde60f6037d3062b7122dec36
Ephraim Godfrey <ephraimgodfrey2002@gmail.com>
2025-05-27 11:50:20 +0530

    add migration

--- alembic/env.py
+++ alembic/env.py
@@ -4,6 +4,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+from backend.models.test_model import TestModel
 
 from backend.core.db import Base 
 
--- backend/models/test_model.py
+++ backend/models/test_model.py
@@ -7,6 +7,7 @@ class TestModel(Base):
     id: int = Column(Integer, primary_key=True, index=True)
     name: str = Column(String, index=True)
     description: str = Column(String, nullable=True)
+    location: str = Column(String, nullable=True)
 
     def __repr__(self):
         return f"<TestModel(id={self.id}, name={self.name}, description={self.description})>"
