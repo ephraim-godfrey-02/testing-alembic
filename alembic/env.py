bbbb6291dfa9ac1bf2018624d7b7e17173aa0770
S Nandan <sureshnandan8@gmail.com>
2025-05-27 12:14:19 +0530

    add migration

--- alembic/env.py
+++ alembic/env.py
@@ -2,10 +2,9 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+from backend.core.db import Base
 
 from alembic import context
-
-from backend.core.db import Base 
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
